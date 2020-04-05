2e3c0d6bb918f3534da9c7d52bdae0be400efcf1
yutakawabata <56316287+yutakawabata@users.noreply.github.com>
2020-04-05 21:04:21 +0900

    Initial Commit

---
+++ CalculatorApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalculatorApp
+//
+//  Created by 川畑雄太 on 2020/04/05.
+//  Copyright © 2020 yuta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CalculatorAppTests/CalculatorAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  CalculatorAppTests.swift
+//  CalculatorAppTests
+//
+//  Created by 川畑雄太 on 2020/04/05.
+//  Copyright © 2020 yuta. All rights reserved.
+//
+
+import XCTest
+@testable import CalculatorApp
+
+class CalculatorAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
